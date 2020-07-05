da501e4286b3a4d54bab0214a30a2a741ac4aa43
Oleg Shiva <shivacode.mail@gmail.com>
2020-07-05 16:18:35 +0300

    tailwind add

---
+++ src/components/Exchanger.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-wrap">
+        <div class="w-1/3 ml-auto bg-gray-500 h-12"></div>
+        <div class="w-1/3 mr-auto bg-gray-400 h-12"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'exchanger'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Exchanger/>
+  </div>
+</template>
+
+<script>
+import Exchanger from '@/components/Exchanger.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Exchanger
+  }
+}
+</script>
